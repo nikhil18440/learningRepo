4931276ebf8d692f98a0688dddc87f212b2a07b5
nikhil <nickjr1032@gmail.com>
2025-09-12 17:55:32 +0530

    created more

---
+++ server.js
@@ -0,0 +1,11 @@
+const http = require('http')
+
+
+const server = http.createServer((req,res) => {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Hello, World!');
+})
+
+server.listen(5000 , () => {
+    console.log("listening on port 5000")
+})
