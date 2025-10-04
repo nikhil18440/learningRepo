4975652251e061ec92ceba65cb8ce150bd5c6cf8
Nikhil k <nickjr1032@gmail.com>
2025-10-04 23:50:56 +0530

    tests

---
+++ sum.test.js
@@ -0,0 +1,14 @@
+const sum = require('./sum.js')
+
+// test('add 1 + 2 equals 3', () => { 
+//     expect(sum(1,2)).toBe(3)
+// })
+
+test('object assignment', () => { 
+    const data = {one:1}
+    data["two"] = 2
+    expect(data).toEqual({
+        one:1,
+        two:2
+    })
+ })
